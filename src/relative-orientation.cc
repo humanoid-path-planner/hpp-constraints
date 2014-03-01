e62997b7f5102b04ad2ad536df094ad897d53917
Florent Lamiraux <florent@laas.fr>
2014-03-01 10:43:22 +0100

    Fix copyright notice.

--- src/relative-orientation.cc
+++ src/relative-orientation.cc
@@ -3,18 +3,18 @@
 // Authors: Florent Lamiraux
 //
 //
-// This file is part of hpp-wholebody-step.
-// hpp-wholebody-step-planner is free software: you can redistribute it
+// This file is part of hpp-constraints.
+// hpp-constraints is free software: you can redistribute it
 // and/or modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation, either version
 // 3 of the License, or (at your option) any later version.
 //
-// hpp-wholebody-step-planner is distributed in the hope that it will be
+// hpp-constraints is distributed in the hope that it will be
 // useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // General Lesser Public License for more details. You should have
 // received a copy of the GNU Lesser General Public License along with
-// hpp-wholebody-step-planner. If not, see
+// hpp-constraints. If not, see
 // <http://www.gnu.org/licenses/>.
 
 #include <hpp/util/debug.hh>
