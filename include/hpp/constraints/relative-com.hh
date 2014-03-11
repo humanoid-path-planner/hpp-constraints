41ca60538c3c1b030ea09a83d6681f43ce69a13e
Florent Lamiraux <florent@laas.fr>
2014-03-11 16:51:50 +0100

    Add a constraint on relative position of center of mass with respect to a joint

---
+++ include/hpp/constraints/relative-com.hh
@@ -0,0 +1,80 @@
+//
+// Copyright (c) 2014 CNRS
+// Authors: Florent Lamiraux
+//
+//
+// This file is part of hpp-constraints.
+// hpp-constraints is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// hpp-constraints is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Lesser Public License for more details. You should have
+// received a copy of the GNU Lesser General Public License along with
+// hpp-constraints. If not, see
+// <http://www.gnu.org/licenses/>.
+
+#ifndef HPP_CONSTRAINTS_RELATIVE_COM_HH
+# define HPP_CONSTRAINTS_RELATIVE_COM_HH
+
+# include <hpp/core/differentiable-function.hh>
+# include <hpp/constraints/config.hh>
+# include <hpp/constraints/fwd.hh>
+
+namespace hpp {
+  namespace eigen {
+    typedef Eigen::Matrix <double, 3, 3> matrix3_t;
+    typedef Eigen::Matrix <double, 3, 1> vector3_t;
+  } // namespace eigen
+  namespace constraints {
+    /// Constraint on the relative position of the center of mass
+    ///
+    /// The value of the function is defined as the position of the center
+    /// of mass in the reference frame of a joint.
+    /// \f{eqnarray*}
+    /// \mathbf{f}(\mathbf{q}) &=& R^T \left(\mathbf{x} - \mathbf{t}\right)
+    /// - \mathbf{x}^{*}\\
+    /// \mathbf{\dot{f}} &=& R^T \left(
+    /// J_{com} + [\mathbf{x}-\mathbf{t}]_{\times}J_{joint}^{\omega}
+    /// - J_{joint}^{\mathbf{v}}\right)\mathbf{\dot{q}}
+    /// \f}
+    /// where
+    /// \li \f$
+    ///     \left(\begin{array}{cc} R & \mathbf{t} \\ 0 & 1\end{array}\right)
+    ///     \f$
+    /// is the position of the joint,
+    /// \li \f$\mathbf{x}\f$ is the position of the center of mass,
+    /// \li \f$\mathbf{x}^{*}\f$ is the desired position of the center of mass
+    ///     expressed in joint frame.
+    class HPP_CONSTRAINTS_DLLAPI RelativeCom : public DifferentiableFunction_t
+    {
+    public:
+      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+      /// Return a shared pointer to a new instance
+      static RelativeComPtr_t create (const DevicePtr_t& robot,
+				      const JointPtr_t& joint,
+				      const vector3_t reference);
+      virtual ~RelativeCom () throw () {}
+    protected:
+      RelativeCom (const DevicePtr_t& robot, const JointPtr_t& joint,
+		   const vector3_t reference);
+      /// Compute value of error
+      ///
+      /// \param argument configuration of the robot,
+      /// \retval result error vector
+      virtual void impl_compute	(vector_t& result,
+				 const vector_t& argument) const throw ();
+      virtual void impl_jacobian (matrix_t &jacobian,
+				  const vector_t &arg) const throw ();
+    private:
+      DevicePtr_t robot_;
+      JointPtr_t joint_;
+      vector3_t reference_;
+      mutable eigen::matrix3_t cross_;
+    }; // class RelativeCom
+  } // namespace constraints
+} // namespace hpp
+#endif // HPP_CONSTRAINTS_RELATIVE_COM_HH
---
+++ src/relative-com.cc
@@ -0,0 +1,96 @@
+//
+// Copyright (c) 2014 CNRS
+// Authors: Florent Lamiraux
+//
+//
+// This file is part of hpp-constraints.
+// hpp-constraints is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// hpp-constraints is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Lesser Public License for more details. You should have
+// received a copy of the GNU Lesser General Public License along with
+// hpp-constraints. If not, see
+// <http://www.gnu.org/licenses/>.
+
+#include <hpp/util/debug.hh>
+#include <hpp/constraints/relative-com.hh>
+#include <hpp/model/device.hh>
+#include <hpp/model/joint.hh>
+
+namespace hpp {
+  namespace eigen {
+    void convert (const constraints::vector3_t& v, model::vector_t& res)
+    {
+      res [0] = v[0]; res [1] = v[1]; res [2] = v[2];
+    }
+
+    void convert (const constraints::matrix3_t& m, matrix3_t& res)
+    {
+      res (0,0) = m (0,0); res (0,1) = m (0,1); res (0,2) = m (0,2);
+      res (1,0) = m (1,0); res (1,1) = m (1,1); res (1,2) = m (1,2);
+      res (2,0) = m (2,0); res (2,1) = m (2,1); res (2,2) = m (2,2);
+    }
+  } // namespace eigen
+
+  namespace constraints {
+    
+    static vector3_t zero3d (0, 0, 0);
+
+    RelativeComPtr_t RelativeCom::create (const DevicePtr_t& robot,
+					  const JointPtr_t& joint,
+					  const vector3_t reference)
+    {
+      RelativeCom* ptr = new RelativeCom (robot, joint, reference);
+      RelativeComShPtr shPtr (ptr);
+      return shPtr;
+    }
+
+    RelativeCom::RelativeCom (const DevicePtr_t& robot, const JointPtr_t& joint,
+			      const vector3_t reference) :
+      DifferentiableFunction (robot->configSize (), robot->numberDof (), 3, "RelativeCom"),
+      robot_ (robot), joint_ (joint), reference_ (reference)
+    {
+      cross_.setZero ();
+    }
+
+    void RelativeCom::impl_compute (vector_t& result,
+				    const vector_t& argument) const throw ()
+    {
+      robot_->currentConfiguration (argument);
+      robot_->computeForwardKinematics ();
+      const Transform3f& M = joint_->currentTransformation ();
+      const vector3_t& x = robot_->positionCenterOfMass ();
+      fcl::Matrix3f RT = M.getRotation (); RT.transpose ();
+      const fcl::Vec3f& t = M.getTranslation ();
+      eigen::convert (RT * (x - t) - reference_, result);
+    }
+
+    void RelativeCom::impl_jacobian (matrix_t &jacobian,
+				     const vector_t &arg) const throw ()
+    {
+      robot_->currentConfiguration (arg);
+      robot_->computeForwardKinematics ();
+      const ComJacobian_t& Jcom = robot_->jacobianCenterOfMass ();
+      const JointJacobian_t& Jjoint (joint_->jacobian ());
+      const Transform3f& M = joint_->currentTransformation ();
+      fcl::Matrix3f RT (M.getRotation ()); RT.transpose ();
+      const vector3_t& x = robot_->positionCenterOfMass ();
+      const vector3_t& t (M.getTranslation ());
+      cross_ (0,1) = -x [2] + t [2]; cross_ (1,0) = x [2] - t [2];
+      cross_ (0,2) = x [1] - t [1]; cross_ (2,0) = -x [1] + t [1];
+      cross_ (1,2) = -x [0] + t [0]; cross_ (2,1) = x [0] - t [0];
+      eigen::matrix3_t eigenRT; eigen::convert (RT, eigenRT);
+      jacobian =
+	eigenRT * (Jcom + cross_ * Jjoint.bottomRows (3) - Jjoint.topRows (3));
+      hppDout (info, "Jcom = " << std::endl << Jcom);
+      hppDout (info, "Jw = " << std::endl << Jjoint.bottomRows (3));
+      hppDout (info, "Jv = " << std::endl << Jjoint.topRows (3));
+    }
+
+  } // namespace constraints
+} // namespace hpp
