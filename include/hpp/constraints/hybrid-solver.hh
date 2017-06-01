945ec80dd9371b65591c289a133fdc6814d8de5f
Joseph Mirabel <jmirabel@laas.fr>
2017-06-01 14:04:54 +0200

    Add class HybridSolver (HierarchicalIterativeSolver + ExplicitSolver)

---
+++ include/hpp/constraints/hybrid-solver.hh
@@ -0,0 +1,63 @@
+#ifndef HPP_CONSTRAINTS_HYBRID_SOLVER_HH
+#define HPP_CONSTRAINTS_HYBRID_SOLVER_HH
+
+#include <vector>
+
+#include <hpp/constraints/fwd.hh>
+#include <hpp/constraints/config.hh>
+
+#include <hpp/constraints/explicit-solver.hh>
+#include <hpp/constraints/iterative-solver.hh>
+
+namespace hpp {
+  namespace constraints {
+    class HPP_CONSTRAINTS_DLLAPI HybridSolver
+      : public HierarchicalIterativeSolver
+    {
+      public:
+        HybridSolver (const std::size_t& argSize, const std::size_t derSize)
+          : HierarchicalIterativeSolver(), explicit_ (argSize, derSize),
+          JeExpanded_ (derSize, derSize)
+        {}
+
+        ExplicitSolver& explicitSolver()
+        {
+          return explicit_;
+        }
+
+        /// Should be called whenever explicit solver is modified
+        void explicitSolverHasChanged();
+
+        template <typename LineSearchType>
+        Status solve (vectorOut_t arg, LineSearchType ls = LineSearchType()) const
+        {
+          // TODO when there are only locked joint explicit constraints,
+          // there is no need for this intricated loop.
+          // if (explicit_.isConstant()) {
+            // explicit_.solve(arg);
+            // iterative_.solve(arg, ls);
+          // } else {
+          return impl_solve (arg, ls);
+          // }
+        }
+
+        inline Status solve (vectorOut_t arg) const
+        {
+          return solve(arg, DefaultLineSearch());
+        }
+
+      private:
+        template <typename LineSearchType>
+        Status impl_solve (vectorOut_t arg, LineSearchType ls) const;
+
+        void updateJacobian (vectorIn_t arg) const;
+
+        ExplicitSolver explicit_;
+        mutable matrix_t Je_, JeExpanded_;
+    }; // class HybridSolver
+  } // namespace constraints
+} // namespace hpp
+
+#include <hpp/constraints/impl/hybrid-solver.hh>
+
+#endif // HPP_CONSTRAINTS_HYBRID_SOLVER_HH
---
+++ include/hpp/constraints/impl/hybrid-solver.hh
@@ -0,0 +1,87 @@
+// Copyright (c) 2017, Joseph Mirabel
+// Authors: Joseph Mirabel (joseph.mirabel@laas.fr)
+//
+// This file is part of hpp-constraints.
+// hpp-constraints is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// hpp-constraints is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// hpp-constraints. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef HPP_CONSTRAINTS_IMPL_HYBRID_SOLVER_HH
+#define HPP_CONSTRAINTS_IMPL_HYBRID_SOLVER_HH
+
+namespace hpp {
+  namespace constraints {
+    template <typename LineSearchType>
+    inline HybridSolver::Status HybridSolver::impl_solve (
+        vectorOut_t arg,
+        LineSearchType lineSearch) const
+    {
+      hppDout (info, "before projection: " << arg.transpose ());
+      assert (!arg.hasNaN());
+
+      explicit_.solve(arg);
+
+      size_type errorDecreased = 3, iter = 0;
+      value_type previousSquaredNorm =
+	std::numeric_limits<value_type>::infinity();
+
+      // Fill value and Jacobian
+      computeValue<true> (arg);
+      computeError();
+
+      hppDout (info, "squareNorm = " << squaredNorm_);
+
+      while (squaredNorm_ > squaredErrorThreshold_ && errorDecreased &&
+	     iter < maxIterations_) {
+
+        // Update the jacobian using the jacobian of the explicit system.
+        updateJacobian(arg);
+
+        computeDescentDirection ();
+        lineSearch (*this, arg, dq_);
+        explicit_.solve(arg);
+
+	computeValue<true> (arg);
+        computeError ();
+
+	hppDout (info, "squareNorm = " << squaredNorm_);
+	--errorDecreased;
+	if (squaredNorm_ < previousSquaredNorm) errorDecreased = 3;
+	previousSquaredNorm = squaredNorm_;
+	++iter;
+
+      }
+
+      hppDout (info, "number of iterations: " << iter);
+      if (squaredNorm_ > squaredErrorThreshold_) {
+	hppDout (info, "Projection failed.");
+        return (!errorDecreased) ? ERROR_INCREASED : MAX_ITERATION_REACHED;
+      }
+      hppDout (info, "After projection: " << arg.transpose ());
+      assert (!arg.hasNaN());
+      return SUCCESS;
+    }
+
+    void HybridSolver::updateJacobian (vectorIn_t arg) const
+    {
+      // Compute Je_
+      explicit_.jacobian(JeExpanded_, arg);
+      explicit_.viewJacobian(JeExpanded_).writeTo(Je_);
+
+      for (std::size_t i = 0; i < stacks_.size (); ++i) {
+        Data& d = datas_[i];
+        d.reducedJ.noalias() += explicit_.outDers().viewTranspose(d.jacobian).eval() * Je_;
+      }
+    }
+  } // namespace constraints
+} // namespace hpp
+
+#endif // HPP_CONSTRAINTS_IMPL_HYBRID_SOLVER_HH
