f6d7e694ff5a5cc73cde8f556bc0b81461fa7b11
Florent Lamiraux <florent@laas.fr>
2018-09-10 11:14:57 +0200

    Fix copyright header.

--- src/explicit/relative-transformation.cc
+++ src/explicit/relative-transformation.cc
@@ -1,18 +1,18 @@
-// Copyright (c) 2017, Joseph Mirabel
+// Copyright (c) 2017 - 2018, Joseph Mirabel
 // Authors: Joseph Mirabel (joseph.mirabel@laas.fr)
 //
-// This file is part of hpp-core.
-// hpp-core is free software: you can redistribute it
+// This file is part of hpp-constraints.
+// hpp-constraints is free software: you can redistribute it
 // and/or modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation, either version
 // 3 of the License, or (at your option) any later version.
 //
-// hpp-core is distributed in the hope that it will be
+// hpp-constraints is distributed in the hope that it will be
 // useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // General Lesser Public License for more details.  You should have
 // received a copy of the GNU Lesser General Public License along with
-// hpp-core. If not, see <http://www.gnu.org/licenses/>.
+// hpp-constraints. If not, see <http://www.gnu.org/licenses/>.
 
 #include <hpp/constraints/explicit/relative-transformation.hh>
 
