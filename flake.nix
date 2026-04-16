cd65b6a86d74cf6f76c3a4d9072c1c2368c6e704
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-16 12:20:32 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,37 +1,27 @@
 {
   description = "Utility classes to check the (robust) equilibrium of a system in contact with the environment.";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.overrideAttrs.hpp-constraints = _: {
-              patches = [ ]; # drop on next release after 7.0.0
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./doc
-                  ./include
-                  ./package.xml
-                  ./src
-                  ./tests
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-constraints = {
+          patches = [ ]; # drop on next release after 7.0.0
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./doc
+              ./include
+              ./package.xml
+              ./src
+              ./tests
+            ];
+          };
+        };
       }
     );
 }
